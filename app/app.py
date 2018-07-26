46d1004b2e084890574fdf3504b12c5c9b7fd50c
Ketan Singh <ketansingh_vsec@yahoo.com>
2018-07-27 01:27:04 +0530

    adding all xref urls lookup for a user

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask
+from flask_jwt import JWT
 from flask_restful import Resource, Api
-from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
+from url import UrlResource
 from user import UserResource
 from users import UsersResource
-from url import UrlResource
+from users_urls_xref import UsersUrlsXrefResource
 
 app = Flask(__name__)
 app.secret_key = "k101011"
@@ -18,12 +19,12 @@ class IndexResource(Resource):
         return "Hello, World!", 200
 
 
-
-
 api.add_resource(IndexResource, "/")
 api.add_resource(UserResource, "/user")
 api.add_resource(UsersResource, "/users")
 api.add_resource(UrlResource, "/shorten")
+api.add_resource(UsersUrlsXrefResource, "/uuxref")
+
 
 if __name__ == "__main__":
     print("Starting MAIN()")
--- app/users_urls_xref.py
+++ app/users_urls_xref.py
@@ -1,4 +1,7 @@
 import sqlite3
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required, current_identity
+from user import User
 
 class Users_Urls_Xref(object):
     def __init__(self, user_id, url_id):
@@ -26,3 +29,36 @@ class Users_Urls_Xref(object):
         cursor.execute(query, (self.user_id, self.url_id))
         connection.commit()
         connection.close()
+
+    @classmethod
+    def get_all_urls_from_user_id(cls, user_id):
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+        query = "SELECT uuxref.url_id, u.long_url, u.short_url FROM users_urls_xref uuxref " \
+                "INNER JOIN urls u ON uuxref.url_id = u.id WHERE uuxref.user_id = ?;"
+        print(query)
+        result = cursor.execute(query, (user_id,))
+
+        rows = result.fetchall()
+        urls = []
+        for row in rows:
+            urls.append({'long_url': row[1], 'short_url': row[2]})
+
+        connection.close()
+        return urls
+
+
+class UsersUrlsXrefResource(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument("username",
+                        type=str,
+                        help="Provide username to list all your urls")
+
+    def get(self):
+        data = UsersUrlsXrefResource.parser.parse_args()
+        user_obj = User.find_user_by_username(data['username'])
+        if user_obj:
+            return {"username": user_obj.username,
+                    "urls": Users_Urls_Xref.get_all_urls_from_user_id(user_obj.id)}, 200
+        else:
+            return {"message": "User with username '{}' not found!".format(data['username'])}, 404
